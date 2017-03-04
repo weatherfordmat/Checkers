c37247cfe845b04c3c44e4e64b22a9863ae0072e
JLDiebel <joshua.l.diebel@gmail.com>
2017-03-04 13:22:25 -0600

    adding api.js

---
+++ public/assets/js/api.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    crud.read();
+});
+
+
+var crud = {
+    read: function() {
+        var id = localStorage.getItem('id');
+        if(id) {
+            var urlId = "https://4qcth52o74.execute-api.us-east-1.amazonaws.com/Test1/api/" + id;
+            $.get({ url: urlId }).then(function(err, results) {
+                if(err) throw err;
+                /*
+var data = {
+"id": 7,
+"name": "Matt W",
+"picture": "https://not-real-url",
+"auth0Key": "343",
+"wins": 0,
+"losses": 0,
+"createdAt": "2017-03-03T02:15:59.000Z",
+"updatedAt": "2017-03-03T02:15:59.000Z"
+}
+            */
+                var data = JSON.parse(results);
+
+                $('.thumbnail').src(data.["picture"]);
+
+               var wins = data.wins / data.wins + data.losses;
+               var color = wins >= 80 ? "green": wins < 80 && >= 60 ? "orange": "red";
+
+                $('.percent').html(wins + "%");
+
+            })
+        } else {
+            console.log("No User Specified.");
+        }
+    },
+    update: function() {
+
+        //run update function
+        this.read();
+
+    },
+    delete: function() {
+
+        //run delete function
+        this.read();
+
+    },
+}

c276b4de3202dff4623ad6ac94a2332ad4cf9a4e
Matthew Weatherford <weatherfordmat@gmail.com>
2017-03-07 17:15:36 -0600

    adding constants.js for import

--- public/assets/js/checkers.js
+++ public/assets/js/checkers.js
@@ -17,7 +17,7 @@ or ~ 8 for better results, but takes a while;
 NOTE: Placing the level of difficulty over 10
 may crash your browser.
 */
-var levelOfDifficulty = 3;
+var levelOfDifficulty = 7;
 
 
 //misc. DOM manipulation;
@@ -30,8 +30,6 @@ $(document).ready(function() {
 
 /*
 Things To Do Today:
-1. Limit Login to FaceBook and Google.
-2. Pick Color Format.
 3. Decide if putting Minesweeper and Tic Tac Toe In/
-4. Error: GET https://secure.gravatar.com/avatar/3d55e9ab31907f2fa4102300e8ab348f?d=404&s=160 404 ()
+7. Add Tests;
 */
